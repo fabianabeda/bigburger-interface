0e9cb7276d39ad1a52702c9a56e84fb81a228415
Fabiana Beda <82904498+fabianabeda@users.noreply.github.com>
2024-08-25 11:53:26 -0300

    Validação pronta

--- src/containers/Login/index.jsx
+++ src/containers/Login/index.jsx
@@ -2,16 +2,14 @@ import { yupResolver } from '@hookform/resolvers/yup';
 import { useForm } from 'react-hook-form';
 import * as yup from 'yup';
 
-
-
 import Logo from '../../assets/logo.svg';
 import {Container, Form, InputContainer, LeftContainer, RightContainer, Title } from './styles';
 import { Button } from '../../components/Button';
 export function Login(){
     const schema = yup
         .object({
-            email: yup.string().email().required(),
-            password: yup.string().min(6).required(),
+            email: yup.string().email('Digite um e-mail válido').required('O e-mail é obrigatório'),
+            password: yup.string().min(6, 'A senha deve ter pelo menos 6 caracteres').required('Digite uma senha'),
         })
         .required()
 
@@ -41,10 +39,12 @@ export function Login(){
                     <InputContainer>
                         <label>Email</label>
                         <input type="email" {...register("email")} />
+                        <p>{errors?.email?.message}</p> 
                     </InputContainer>
                     <InputContainer>
                         <label>Senha</label>
                         <input type="password" {...register("password")}/>
+                        <p>{errors?.password?.message}</p> 
                     </InputContainer>
                     <Button type="submit">Entrar</Button>
                 </Form>
