792ba54b224da3ccdb491ac62d25ce615b63a4e3
Fabiana Beda <82904498+fabianabeda@users.noreply.github.com>
2024-09-02 15:12:48 -0300

    Usuário criado

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client'
 import { ToastContainer } from 'react-toastify';
-import { Login } from './containers/Login';
+import { RouterProvider } from 'react-router-dom';
 import GlobalStyle from './styles/globalStyles';
+import { router } from './router';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <Login/>
+    <RouterProvider router={router} />
     <GlobalStyle />
     <ToastContainer autoClose={2000} theme="colored" />
   </React.StrictMode>,
