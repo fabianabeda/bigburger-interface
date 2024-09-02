99d6cac033f6fa4fe09e808088c2fb2c9f76cd0b
Fabiana Beda <82904498+fabianabeda@users.noreply.github.com>
2024-09-02 16:08:34 -0300

    Criei tela de registro

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client'
 import { ToastContainer } from 'react-toastify';
 import { RouterProvider } from 'react-router-dom';
 import GlobalStyle from './styles/globalStyles';
-import { router } from './router';
+import { router } from './routes';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
