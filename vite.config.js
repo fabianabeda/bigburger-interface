3c8df0859f5a465a9e2174d8910dec193f5bd561
Fabiana Beda <82904498+fabianabeda@users.noreply.github.com>
2025-06-10 19:03:28 -0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/bigburger-interface/', // ← ESSENCIAL para GitHub Pages
   plugins: [react()],
 })
+
